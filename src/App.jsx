e7ea7294c2b3d8cebb53f0c22d740973f8262f33
YILDIZ TUNAR <ozdemirryildzz@gmail.com>
2024-09-03 14:44:43 +0400

    carousel and button ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { CarouselDemo } from "./components/demo/CarouselDemo"
+import { Button } from "./components/ui/button"
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-          <div className="text-xl font-bold underline">
-          Hello world!
-          </div>
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="px-20">
+      <Button>Hello Shadcn</Button>
+      <CarouselDemo></CarouselDemo>
+    </div>
     </>
   )
 }
